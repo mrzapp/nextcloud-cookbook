3a49cc1d2520ebb4aec15d5c861da45c6407a5fa
Christian Wolf <github@christianwolf.email>
2021-07-13 21:06:40 +0200

    Create first strucutre to test migration issue, implementation issue with PHPUnit
    Signed-off-by: Christian Wolf <github@christianwolf.email>

---
+++ tests/Integration/Setup/Migrations/Version000000Date20210701093123Test.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace tests\Integration\Setup\Migrations;
+// namespace OCA\Cookbook\tests\Integration\Setup\Migrations;
+
+use OCP\IDBConnection;
+use OCP\AppFramework\App;
+use OCP\AppFramework\IAppContainer;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OC\DB\SchemaWrapper;
+use PHPUnit\Framework\TestCase;
+
+class Version000000Date20210701093123Test extends TestCase {
+    
+    /**
+     * @var IAppContainer
+     */
+    private $container;
+    
+    /**
+     * @var IDBConnection
+     */
+    private $db;
+    
+    public function setUp(): void {
+        resetEnvironmentToBackup('default', true);
+        
+        parent::setUp();
+        
+        $app = new App('cookbook');
+        $this->container = $app->getContainer();
+        
+        /**
+         * @var IDBConnection $db
+         */
+        $this->db = $this->container->query(IDBConnection::class);
+        $this->assertIsObject($this->db);
+        /**
+         * @var SchemaWrapper $schema
+         */
+        $schema = $this->container->query(SchemaWrapper::class);
+        $this->assertIsObject($schema);
+        
+        print_r($this->db->getQueryBuilder()->select('version')->from('migrations')->where('app=:app')->setParameter('app', 'cookbook')->execute()->fetchAll());
+        runOCCCommand(['migration:status', 'cookbook'], true);
+        
+        // undo all migrations of cookbook app
+        $qb = $this->db->getQueryBuilder();
+        $numRows = $qb->delete('migrations')
+            ->where('app=:app')
+            ->setParameter('app', 'cookbook')
+            ->execute();
+        $this->assertGreaterThan(0, $numRows);
+        
+        $schema->dropTable('cookbook_names');
+        $this->assertFalse($schema->hasTable('cookbook_names'));
+        $schema->dropTable('cookbook_categories');
+        $this->assertFalse($schema->hasTable('cookbook_categories'));
+        $schema->dropTable('cookbook_keywords');
+        $this->assertFalse($schema->hasTable('cookbook_keywords'));
+        
+        $schema->performDropTableCalls();
+        
+        // Reinstall app partially (just before the migration)
+        runOCCCommand(['migration:status', 'cookbook'], true);
+        runOCCCommand(['migration:migrate', 'cookbook', '000000Date20210427082010']);
+        runOCCCommand(['migration:status', 'cookbook'], true);
+    }
+    
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testRedundantEntriesInDB($data, $updatedUsers) {
+        // Add dummy data
+        $qb = $this->db->getQueryBuilder();
+        $qb->insert('cookbook_names')
+            ->values([
+                'recipe_id' => ':recipe',
+                'user_id' => ':user',
+                'name' => ':name',
+            ]);
+        $qb->setParameter('name', 'name of the recipe');
+        foreach ($data as $d) {
+            $qb->setParameter('user', $d[0]);
+            $qb->setParameter('recipe', $d[1]);
+            
+            $this->assertEquals(1, $qb->execute());
+        }
+        
+        // Set configuration values
+        $current = time();
+        
+        $qb = $this->db->getQueryBuilder();
+        $qb->insert('preferences')
+            ->values([
+                'userid' => ':user',
+                'appid' => ':appid',
+                'configkey' => ':property',
+                'configvalue' => ':value',
+            ]);
+        
+        $qb->setParameter('value', $current, IQueryBuilder::PARAM_STR);
+        $qb->setParameter('appid', 'cookbook');
+        $qb->setParameter('property', 'last_index_update');
+        
+        $users = array_unique(array_map(function ($x) {return $x[0];}, $data));
+        foreach($users as $u){
+            $qb->setParameter('user', $u);
+            $this->assertEquals(1, $qb->execute());
+        }
+        
+        runOCCCommand(['migration:migrate', 'cookbook', '000000Date20210701093123']);
+        
+        $qb = $this->db->getQueryBuilder();
+        $qb->select('userid', 'configvalue')
+            ->from('preferences')
+            ->where(
+                'appid = :appid',
+                'configkey = :property'
+                );
+        $qb->setParameter('appid', 'cookbook');
+        $qb->setParameter('property', 'last_index_update');
+        
+        $cursor = $qb->execute();
+        $result = $cursor->fetchAll();
+        
+        print_r($result);
+        $result = array_filter($result, function ($x) use ($current) { return $x < $current; });
+        print_r($result);
+        
+        $this->assertEquals(count($updatedUsers), count($result));
+        $this->assertEquals(sort($updatedUsers), sort($result));
+        
+        $this->markTestIncomplete('Not yet implemented');
+    }
+    
+    public function dataProvider() {
+        return [
+//             'case A' => [
+//                 [
+//                     ['alice', 123],
+//                     ['alice', 124],
+//                     ['bob', 125]
+//                 ],
+//                 [],
+//             ],
+//             'case B' => [
+//                 [
+//                     ['alice', 123],
+//                     ['alice', 124],
+//                     ['bob', 124],
+//                     ['bob', 125],
+//                 ],
+//                 [],
+//             ],
+            'case C' => [
+                [
+                    ['alice', 123],
+                    ['alice', 124],
+                    ['bob', 124],
+                    ['bob', 124],
+                    ['bob', 125],
+                ],
+                ['bob']
+            ],
+        ];
+    }
+}
